d3a3bb8030eb169d78467bc46e7450c6d6f67a98
khushi <nagar.khushi2001@gmail.com>
2023-05-23 13:00:52 +0530

    third commit

---
+++ jsProgram1.js
@@ -0,0 +1,55 @@
+//1
+
+
+// //find out area of a tringle by heron's formula = sqrt(s*(s-a)*(s-b)*(s-c))
+// let a,b,c,s;
+// a=5;
+// b=6;
+// c=7;
+// s=(a+b+c)/2;
+// const area= Math.sqrt(s*(s-a)*(s-b)*(s-c)); // Math.sqrt is use for square root
+
+// console.log(area); //print value 
+
+
+
+// 2
+// find the today's date 
+// var today = new Date();
+// var month = today.getMonth();
+// var year = today.getFullYear();
+// var date = today.getDate();
+// let current_date = `${month}/${year}/${date}`;
+// console.log(current_date);
+
+
+//3 
+//find the today's day
+// var today = new Date();
+// var day = today.getDay();  getDay is method 
+// var dayList = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+// console.log("today is :"+ dayList[day] + ".");
+
+
+//4
+// find the current time 
+// var today = new Date();
+// var hour = today.getHours();
+// var minute = today.getMinutes();
+// var second = today.getSeconds();
+// var Time1 = (`${hour}:${minute}:${second} PM`);   $= query selector 
+// var Time2 = (`${hour}:${minute}:${second} AM`);      `concatinate = add values as a string `
+
+// console.log(hour>=12?Time1:Time2);
+
+
+
+//5
+//division and multiply
+// let a=25; 
+// let b=5;
+// let c=a*b ;
+//  let d=a/b;
+
+// console.log(c);
+// console.log(d);
